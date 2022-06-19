1e10a4d83076a2b240c4f0022d2118f754178926
personal <samuelkupoe1@gmail.com>
2022-06-19 19:14:02 +0000

    edited

--- coldstoreapp/src/components/ColdStoreNavbar.js
+++ coldstoreapp/src/components/ColdStoreNavbar.js
@@ -1,24 +1,36 @@
 // import * as React from 'react';
-import {AppBar, Toolbar, IconButton, Typography, Stack, Button} from '@mui/material';
-import SetMealTwoToneIcon from '@mui/icons-material/SetMealTwoTone';
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Stack,
+  Button,
+} from "@mui/material";
+import SetMealTwoToneIcon from "@mui/icons-material/SetMealTwoTone";
 
-
- function ColdStoreNavbar() {
+function ColdStoreNavbar() {
   return (
     <AppBar position="static">
-     <Toolbar>
-      <IconButton size="large" edge="start" color="info" aria-label="logo">
-      <SetMealTwoToneIcon style={{color: "white", width: "30", height: "30"}}/>
-      </IconButton>
-      <Typography variant="h6" component="div"  color= "white" sx={{flexGrow: 1}}>
-        Smart Tuna
-      </Typography>
-      <Stack direction="row" spacing={2}>
-      <Button color="inherit">Login</Button>
-      </Stack>
-     </Toolbar>
+      <Toolbar>
+        <IconButton size="large" edge="start" color="info" aria-label="logo">
+          <SetMealTwoToneIcon
+            style={{ color: "white", width: "30", height: "30" }}
+          />
+        </IconButton>
+        <Typography
+          variant="h6"
+          component="div"
+          color="white"
+          sx={{ flexGrow: 1 }}
+        >
+          Smart Tuna
+        </Typography>
+        <Stack direction="row" spacing={2}>
+          <Button color="inherit">Login</Button>
+        </Stack>
+      </Toolbar>
     </AppBar>
-  
-  )
+  );
 }
 export default ColdStoreNavbar;
