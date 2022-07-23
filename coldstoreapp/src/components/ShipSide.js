5d63d9c7ba64c1b07af76d40de56d8a3d22d5026
personal <samuelkupoe1@gmail.com>
2022-07-23 20:21:40 +0000

    shipSide updated

--- coldstoreapp/src/components/ShipSide.js
+++ coldstoreapp/src/components/ShipSide.js
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Button } from "react-bootstrap";
 import "../css/ShipSide.css";
 import Form from "react-bootstrap/Form";
-
-
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemHeading,
+  AccordionItemButton,
+  AccordionItemPanel,
+} from 'react-accessible-accordion';
+import 'react-accessible-accordion/dist/fancy-example.css';
 
 class ShipSide extends Component {
   constructor(props) {
@@ -21,18 +27,28 @@ class ShipSide extends Component {
     alert("Data was submitted: " + this.state.value);
     event.preventDefault();
   }
- 
+
   render() {
     const data = [
       { scrow: "0008", type: "Tuna", net: 1080 },
       { scrow: "0009", type: "Salmon", net: 1140 },
-      { scrow: "0010", type: "Cat Fish", net: 1080},
-    ]
+      { scrow: "0010", type: "Cat Fish", net: 1080 },
+    ];
+
+    
     return (
       <div className="ship-side">
         <Row style={{ paddingTop: "50px" }}>
           <Col>
-            <form style={{boxShadow: "2px 3px 10px 1px #000", marginLeft: "20px", margin: "20px", padding: "15px ", paddingRight: "-18px"}}>
+            <form
+              style={{
+                boxShadow: "2px 3px 10px 1px #000",
+                marginLeft: "20px",
+                margin: "20px",
+                padding: "15px ",
+                paddingRight: "-18px",
+              }}
+            >
               <div className="form-group">
                 <label for="snumber">Scow Number</label>
                 <input
@@ -50,78 +66,172 @@ class ShipSide extends Component {
                 <label for="grossweight">Gross Weight</label>
                 <input type="number" class="form-control" id="grossweight" />
               </div>
-              <div className="form-group" >
-              <Form.Label className="quotesFormLabelText" style={{marginBottom: "0"}}>
-             Fish Type
-            </Form.Label>
-            <Form.Select
-              aria-label="Default select example"
-              className="quotesFormsControl"
-            >
-              <option value="Mobile Apps">Tuna</option>
-              <option value="Website">Salmon</option>
-              <option value="Software">Herrings</option>
-              <option value="Other">Other</option>
-            </Form.Select>
-                
+              <div className="form-group">
+                <Form.Label
+                  className="quotesFormLabelText"
+                  style={{ marginBottom: "0" }}
+                >
+                  Fish Type
+                </Form.Label>
+                <Form.Select
+                  aria-label="Default select example"
+                  className="quotesFormsControl"
+                >
+                  <option value="Mobile Apps">Tuna</option>
+                  <option value="Website">Salmon</option>
+                  <option value="Software">Herrings</option>
+                  <option value="Other">Other</option>
+                </Form.Select>
               </div>
               <div className="form-group">
-              <Form.Label className="quotesFormLabelText" style={{marginBottom: "0"}}>
-              Cold Store
-             </Form.Label>
-             <Form.Select
-               aria-label="Default select example"
-               className="quotesFormsControl"
-             >
-               <option value="Mobile Apps">Main</option>
-               <option value="Website">Sub-Station</option>
-               <option value="Software">West Side</option>
-               <option value="Other">Market</option>
-             </Form.Select>
+                <Form.Label
+                  className="quotesFormLabelText"
+                  style={{ marginBottom: "0" }}
+                >
+                  Cold Store
+                </Form.Label>
+                <Form.Select
+                  aria-label="Default select example"
+                  className="quotesFormsControl"
+                >
+                  <option value="Mobile Apps">Main</option>
+                  <option value="Website">Sub-Station</option>
+                  <option value="Software">West Side</option>
+                  <option value="Other">Market</option>
+                </Form.Select>
               </div>
-                 <div className="form-group">
-                 <h4>Net Weight    0000</h4>
-                 </div>
+              <div className="form-group">
+                <h4>Net Weight 0000</h4>
+              </div>
               <button type="submit" class="btn btn-primary" id="form-group">
                 ADD
               </button>
             </form>
           </Col>
-          <Col className="table">
-          <div>
-          <table>
-           <tr>
-           <th>Cold Store1</th>
-           <th></th>
-           <th>3</th>
+          <Col className="collapsible">
+          <Accordion>
+          <AccordionItem>
+              <AccordionItemHeading>
+                  <AccordionItemButton>
+                     COLDSTORE 1  
+                  </AccordionItemButton>
+              </AccordionItemHeading>
+              <AccordionItemPanel>
            
-           </tr>
-            <tr>
-              <th style={{fontSize: 10}}>Scrow Number</th>
-              <th style={{fontSize: 10}}>Fish Type</th>
-              <th style={{fontSize: 10}}>Net Weight</th>
+              <table>
+        <tr>
+          <th>Scrow No:</th>
+          <th>Fish Type</th>
+          <th>Net Weight</th>
+        </tr>
+        {data.map((val, key) => {
+          return (
+            <tr key={key}>
+              <td>{val.scrow}</td>
+              <td>{val.type}</td>
+              <td>{val.net}</td>
             </tr>
-            {data.map((val, key) => {
-              return (
-                <tr key={key}>
-                  <td>{val.scrow}</td>
-                  <td>{val.type}</td>
-                  <td>{val.net}</td>
-                </tr>
-              )
-            })}
-          </table>
-          <button style={{fontSize: 12}} type="submit" class="btn btn-primary" id="form-group">
-                Dispatch
-              </button>
-        </div>
-          
+          )
+        })}
+      </table>
+      <Button style={{marginLeft: 100, marginTop: 5}}>Dispatch</Button>
+                 
+                  
+              </AccordionItemPanel>
+          </AccordionItem>
+          <AccordionItem>
+              <AccordionItemHeading>
+                  <AccordionItemButton>
+                     COLDSTORE 2
+                  </AccordionItemButton>
+              </AccordionItemHeading>
+              <AccordionItemPanel>
+              <table>
+              <tr>
+                <th>Scrow No:</th>
+                <th>Fish Type</th>
+                <th>Net Weight</th>
+              </tr>
+              {data.map((val, key) => {
+                return (
+                  <tr key={key}>
+                    <td>{val.scrow}</td>
+                    <td>{val.type}</td>
+                    <td>{val.net}</td>
+                  </tr>
+                )
+              })}
+            </table>
+            <Button style={{marginLeft: 100, marginTop: 5}}>Dispatch</Button>
+              </AccordionItemPanel>
+          </AccordionItem>
+          <AccordionItem>
+              <AccordionItemHeading>
+                  <AccordionItemButton>
+                     COLDSTORE 3
+                  </AccordionItemButton>
+              </AccordionItemHeading>
+              <AccordionItemPanel>
+              <table>
+              <tr>
+                <th>Scrow No:</th>
+                <th>Fish Type</th>
+                <th>Net Weight</th>
+              </tr>
+              {data.map((val, key) => {
+                return (
+                  <tr key={key}>
+                    <td>{val.scrow}</td>
+                    <td>{val.type}</td>
+                    <td>{val.net}</td>
+                  </tr>
+                )
+              })}
+            </table>
+            <Button style={{marginLeft: 100, marginTop: 5}}>Dispatch</Button>
+              </AccordionItemPanel>
+          </AccordionItem>
+          <AccordionItem>
+              <AccordionItemHeading>
+                  <AccordionItemButton>
+                     COLDSTORE 4
+                  </AccordionItemButton>
+              </AccordionItemHeading>
+              <AccordionItemPanel>
+              <table>
+              <tr>
+                <th>Scrow No:</th>
+                <th>Fish Type</th>
+                <th>Net Weight</th>
+              </tr>
+              {data.map((val, key) => {
+                return (
+                  <tr key={key}>
+                    <td>{val.scrow}</td>
+                    <td>{val.type}</td>
+                    <td>{val.net}</td>
+                  </tr>
+                )
+              })}
+            </table>
+            <Button style={{marginLeft: 100, marginTop: 5}}>Dispatch</Button>
+              </AccordionItemPanel>
+          </AccordionItem>
+      </Accordion>
           </Col>
           <Col>
-          <form style={{boxShadow: "2px 3px 10px 1px #000",height: "70vh", margin: "20px", padding: "px ", paddingRight: ""}}>
+            <form
+              style={{
+                boxShadow: "2px 3px 10px 1px #000",
+                height: "70vh",
+                margin: "20px",
+                padding: "px ",
+                paddingRight: "",
+              }}
+            >
               <div className="form-group">
-                 <h5 style={{textAlign: "center"}}>Summary</h5>
-                </div>
+                <h5 style={{ textAlign: "center" }}>Summary</h5>
+              </div>
             </form>
           </Col>
         </Row>
